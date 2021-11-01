6a7d7bbf6024634a1db0cca681183dc6e4e188f6
Demi Marie Obenour <demi@invisiblethingslab.com>
2021-11-01 11:04:55 -0400

    Fix typo in main.c
    This was harmless as-is but could cause problems later, as popups would
    not be destroyed.

--- cbits/main.c
+++ cbits/main.c
@@ -112,7 +112,7 @@ void qubes_give_view_keyboard_focus(struct tinywl_view *view, struct wlr_surface
 					seat->keyboard_state.focused_surface);
 		if (previous->role == WLR_XDG_SURFACE_ROLE_TOPLEVEL) {
 			wlr_xdg_toplevel_set_activated(previous, false);
-		} else if (previous->role == WLR_XDG_SURFACE_ROLE_TOPLEVEL) {
+		} else if (previous->role == WLR_XDG_SURFACE_ROLE_POPUP) {
 			wlr_xdg_popup_destroy(previous);
 		}
 	}
