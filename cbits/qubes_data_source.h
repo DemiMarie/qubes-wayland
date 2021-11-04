349e0a7979f51015d239b8866bd8cd57b831ae0b
Demi Marie Obenour <demi@invisiblethingslab.com>
2021-11-04 04:54:02 -0400

    Implement pasting of data provided by GUI daemon
    As with copying, this is implemented in an entirely asynchronous,
    non-blocking manner.

---
+++ cbits/qubes_data_source.h
@@ -0,0 +1,6 @@
+#ifndef QUBES_WAYLAND_COMPOSITOR_DATA_SOURCE_H
+#define QUBES_WAYLAND_COMPOSITOR_DATA_SOURCE_H _Pragma("GCC error \"double-include guard used\"")
+#include "common.h"
+
+struct qubes_data_source *qubes_data_source_create(struct wl_display *display, uint32_t len, const uint8_t *ptr);
+#endif
