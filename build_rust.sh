1ce2cfb81de23120f1efba174c216d7cc0c904ed
Demi Marie Obenour <demi@invisiblethingslab.com>
2022-11-12 23:07:16 -0500

    Fix out-of-source-tree build directories
    This didn’t build the Rust code properly.

--- build_rust.sh
+++ build_rust.sh
@@ -1,12 +1,12 @@
 #!/bin/sh --
 set -eu
 unset outdir target_dir mode
-depfile=$1 outdir=$2 target_dir=$3 mode=$4
-shift 4
+depfile=$1 outdir=$2 target_dir=$3 path=$4 mode=$5
+shift 5
 case $mode in
 (debug|release) :;;
 (*) echo 'Mode must be debug or release'>&2; exit 1;;
 esac
-cargo build "--target-dir=$target_dir" "--$mode" "$@" &&
+cargo build "--target-dir=$target_dir" "--$mode" "--manifest-path=$path/Cargo.toml" "$@" &&
 cp -- "$target_dir/$mode/libqubes_gui_rust.a" "$outdir/" &&
 cp -- "$target_dir/$mode/libqubes_gui_rust.d" "$depfile"
