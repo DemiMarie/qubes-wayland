5ba1b1258e21a2fe69ac2c69722fea5be1991e81
Demi Marie Obenour <demi@invisiblethingslab.com>
2022-11-16 18:34:56 -0500

    Move function use after its definition
    No change in behavior.

--- cbits/qubes_xwayland.c
+++ cbits/qubes_xwayland.c
@@ -90,24 +90,8 @@ static void xwayland_surface_unmap(struct wl_listener *listener, void *data)
 }
 
 static void xwayland_surface_set_size(struct qubes_xwayland_view *view,
-	                                   int32_t x,
-	                                   int32_t y,
-	                                   uint32_t width,
-	                                   uint32_t height);
-
-static void xwayland_surface_request_configure(struct wl_listener *listener, void *data)
-{
-	struct qubes_xwayland_view *view = wl_container_of(listener, view, request_configure);
-	struct wlr_xwayland_surface_configure_event *event = data;
-
-	wlr_xwayland_surface_configure(view->xwayland_surface, event->x, event->y, event->width, event->height);
-}
-
-static void xwayland_surface_set_size(struct qubes_xwayland_view *view,
-	                                   int32_t x,
-	                                   int32_t y,
-	                                   uint32_t width,
-	                                   uint32_t height) {
+                                      int32_t x, int32_t y,
+                                      uint32_t width, uint32_t height) {
 	struct qubes_output *output = &view->output;
 	wlr_log(WLR_DEBUG, "configuring surface at %p", view);
 	assert(QUBES_XWAYLAND_MAGIC == view->output.magic);
@@ -119,6 +103,14 @@ static void xwayland_surface_set_size(struct qubes_xwayland_view *view,
 		return; /* cannot handle this */
 	}
 	qubes_send_configure(output, width, height);
+}
+
+static void xwayland_surface_request_configure(struct wl_listener *listener, void *data)
+{
+	struct qubes_xwayland_view *view = wl_container_of(listener, view, request_configure);
+	struct wlr_xwayland_surface_configure_event *event = data;
+
+	wlr_xwayland_surface_configure(view->xwayland_surface, event->x, event->y, event->width, event->height);
 }
 
 static void xwayland_surface_request_move(struct wl_listener *listener, void *data) {
